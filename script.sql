9bd1c14978e1ad762106f6e665fedb5e64191c8f
Joabe Oliveira & Julia Cunha <julia.tassini@fatec.sp.gov.br>
2023-09-26 11:05:42 -0300

    feat(cur-unbound):calculo de preco medio por pais

--- script.sql
+++ script.sql
@@ -1,8 +1,25 @@
--- Criando uma tabela
-CREATE TABLE tb_vinhos (
+-- 2 - Cursor não vinculado
+DO $$
+DECLARE
+	cur_preco_medio_pais REFCURSOR;
+	v_preco_medio NUMERIC(5,2);
+	v_pais VARCHAR(200);
+BEGIN
+	OPEN cur_preco_medio_pais FOR SELECT country, AVG(price) FROM tb_vinhos2 GROUP BY country; 
+	LOOP 
+		FETCH cur_preco_medio_pais INTO v_pais, v_preco_medio;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE 'O país é % e a média é %',v_pais, v_preco_medio;
+	END LOOP;
+	CLOSE cur_preco_medio_pais;
+END; 
+$$
+
+-- 1 - Criando a tabela
+CREATE TABLE tb_vinhos2 (
 	ID SERIAL PRIMARY KEY,
 	country VARCHAR(200),
-	description VARCHAR(500),
+	description VARCHAR(1000),
 	points INT,
 	price NUMERIC
 );
